ff0edefcdec3239c8d4f5b5338559927bcb9532a
Raul Almeida Lopez <raulalmeidalopez@gmail.com>
2023-06-13 16:18:06 -0400

    Se sustituyen las funciones de retardo por la variante de selenium explicityWait, para optimizar los tiempos de ejecucion y prevenir algunos errores de sincronismo

--- All_Recipes_Scraping.py
+++ All_Recipes_Scraping.py
@@ -1,9 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import (NoSuchElementException,TimeoutException)
 from selenium.webdriver.common.by import By
 import time
+from selenium.webdriver.support.wait import WebDriverWait
+
 
 def main():
     
@@ -48,7 +50,7 @@ def recipes_info():
 
     # Crea una instancia del navegador por defecto.
     driver = webdriver.Firefox()
-    time.sleep(5)
+    time.sleep(1)
 
     count = 0
 
@@ -63,36 +65,33 @@ def recipes_info():
         # Accede a la página.
         driver.get(recipe_link)
 
-        # Se espera para que se carguen todos los elementos de la página.
-        #! Depende de la velocidad de internet, pero aumenta mucho la espera y demora la ejecución del programa.
-
-        time.sleep(10)
-        
         # Manejo de excepciones imprevistas quedarán anotadas las recetas que las provoquen en un txt.
-        try:
-            """
-            #//    # Manejo de excepciones en caso de que no exista botón de más comentarios en una página.
-            #//    try:
-            #//        button = driver.find_element(By.CLASS_NAME, "feedback-list__load-more-button")
-            #//        # Si es posible se presiona el botón determinadas veces.
-            #//        for i in range(1):
-            #//            button.click()
-            #//            # Espera a que se cargue el contenido.
-            #//            # driver.implicitly_wait(20) 
-            #//            time.sleep(5)   
-            #//    except NoSuchElementException:
-            #//        pass
-            """            
-            # Se carga el contenido del html.
+        try: 
+            
+            # Manejo de excepciones en caso de que no exista botón de más comentarios en una página.
+            try:
+                
+                #Si es posible se presiona el botón determinadas veces.
+                for i in range(2):
+                    #Se espera a que cargue el boton, o un max de 20s
+                    button = WebDriverWait(driver, timeout=20).until(lambda d: d.find_element(By.CLASS_NAME,"feedback-list__load-more-button")) 
+                    button.click()    
+
+            except TimeoutException :
+                pass
+                        
+            # Se carga el contenido del html
             html_recipe_info = driver.page_source
 
             soup_recipe_info = BeautifulSoup(html_recipe_info, "lxml")
             
             recipe_name = soup_recipe_info.find("h1", class_ = "comp type--lion article-heading mntl-text-block").text
+
             try:
                 rating = soup_recipe_info.find("div", id = "mntl-recipe-review-bar__rating_1-0").text
             except:
                 rating = "0.0"
+
             # Tab que contiene la info de tiempos de preparación.
             all_times_info = soup_recipe_info.find_all("div", class_ = "mntl-recipe-details__item")
             
@@ -107,11 +106,6 @@ def recipes_info():
             
             ingredients_list = [x.text for x in ingredients_tags]
             
-            print(recipe_name.strip())
-            print(rating.strip())
-            print(total_time.strip())
-            print(ingredients_list)
-
             with open("Ingredients.txt", "a") as f_ing:
                 for ingredient in ingredients_list:
                     f_ing.write(ingredient)
@@ -129,22 +123,28 @@ def recipes_info():
                 value = row.find("td", class_ = "mntl-nutrition-facts-summary__table-cell type--dog-bold").text
                 nutrition_facts.update({key:value})
 
+            # Se espera a que carguen los comentarios
+            WebDriverWait(driver, timeout=5).until(lambda d: d.find_element(By.CLASS_NAME,"feedback__text"))
             feedback_list_tags = soup_recipe_info.find_all("div", class_ = "feedback__text")
 
             if len(feedback_list_tags) > 0:
 
                 feedback_list = [x.p.text for x in feedback_list_tags]
-                
+
                 # Se guardan los comentarios, separados por 2 lineas en blanco.
                 with open("Feedbacks.txt", "w") as f_feed:
                     for comment in feedback_list:
                         f_feed.write(f"{comment}\n\n")
                         print(comment + "\n\n")  
+            
+            print(recipe_name.strip())
+            print(rating.strip())
+            print(total_time.strip())
+            print(ingredients_list)
 
-        except KeyError as e:
+        except :
             with open("Bad_links.txt", "w") as errors:
                 errors.write(recipe_link + "\n")
-                errors.write(str(e))
             continue
 
     f.close()
